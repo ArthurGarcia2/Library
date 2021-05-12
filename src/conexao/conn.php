2f3992fd8871a0c1b30d1166ba4c2c5e0f1a9f79
ArthurGarcia2 <arthurg4rcia44@gmail.com>
2021-05-12 14:16:08 -0300

    enviando atualização de rotina.

---
+++ src/conexao/conn.php
@@ -0,0 +1,14 @@
+<?php
+
+    $hostmane = "fdb29.awardspace.net";
+    $dbname = "3802844_library";
+    $username = "3802844_library";
+    $password = "Ewp816204";
+
+    try {
+        $pdo = new PDO('mysql:host='.$hostmane.';dbname='.$dbname, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //echo 'Conexão Bem Sucedida';
+    } catch (PDOException $e) {
+        echo 'Conexão Mal Sucedida, Erro: '.$e->getMessage();
+    }
